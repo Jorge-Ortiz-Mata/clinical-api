5afddd45f8bd63d532c65125a9ceca658feebcf8
jorge-ortiz-sp <jorge.ortiz@sailpoint.com>
2025-05-04 15:50:34 -0600

    Create and migrate database

--- db/migrate/20200925203025_create_doctors.rb
+++ db/migrate/20200925203025_create_doctors.rb
@@ -1,10 +1,10 @@
 class CreateDoctors < ActiveRecord::Migration[6.0]
   def change
-    create_table :doctors do |t|
-      t.string :name
-      t.string :email
+    # create_table :doctors do |t|
+    #   t.string :name
+    #   t.string :email
 
-      t.timestamps
-    end
+    #   t.timestamps
+    # end
   end
 end
